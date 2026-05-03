c3deeef12991fbc1dc4a12e4e9efd93d58d332be
Letícia Videira Gois <leticia.videira.gois@gmail.com>
2026-05-03 17:12:35 -0300

    Adição de funções uteis

--- src/formas/forma.h
+++ src/formas/forma.h
@@ -13,9 +13,23 @@ typedef void* FORMA;
 
 FORMA criarForma(FormaTipo tipo, void *data);
 
+char* getNomeForma (FORMA f);
+
+int getIdForma(FORMA f);
+
+void getAncoraForma(FORMA f, double *x, double *y);
+
 FormaTipo getTipoForma(FORMA f);
 
 void* getDataForma(FORMA f);
+
+void setCorBordaForma(FORMA f, char *cor);
+
+void setCorPreenchimentoForma(FORMA f, char *cor);
+
+void moverForma(FORMA f, double dx, double dy);
+
+void printFormaTXT(FILE *txt, FORMA f);
 
 void killForma(FORMA f);
 
